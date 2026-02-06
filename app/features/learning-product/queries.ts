738281e30fba6478713532594825eb00cc421315
neodev2026 <neo.dev.2026@gmail.com>
2026-02-06 17:21:34 +0100

    Implement learning product detail page

--- app/features/learning-product/queries.ts
+++ app/features/learning-product/queries.ts
@@ -2,7 +2,7 @@ import type { SupabaseClient } from "@supabase/supabase-js";
 import type { Database } from "database.types";
 
 /**
- * [wemake Style] 활성화된 모든 학습 제품(단어장) 목록을 조회합니다.
+ *  활성화된 모든 학습 제품(단어장) 목록을 조회합니다.
  */
 export const getLearningProducts = async (client: SupabaseClient<Database>) => {
   const { data, error } = await client
@@ -14,3 +14,20 @@ export const getLearningProducts = async (client: SupabaseClient<Database>) => {
   if (error) throw error;
   return data;
 };
+
+/**
+ * ID를 통해 특정 학습 제품의 상세 정보를 조회합니다.
+ */
+export const getProductById = async (
+    client: SupabaseClient<Database>,
+    { productId }: { productId: string }
+  ) => {
+    const { data, error } = await client
+      .from("learning_product")
+      .select("*")
+      .eq("id", productId)
+      .single();
+  
+    if (error) throw error;
+    return data;
+  };
